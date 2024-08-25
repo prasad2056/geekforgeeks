fdfe44057674d2d5a86708e4bbe020901ec16b7f
PRASAD.G <147723021+prasad2056@users.noreply.github.com>
2024-08-25 19:27:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        StringBuilder sb = new StringBuilder();
+        for (int e : a) sb.append(e + " ");
+        System.out.println(sb);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str[] = br.readLine().trim().split(" ");
+
+            int n = Integer.parseInt(str[0]);
+            int s = Integer.parseInt(str[1]);
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(arr, n, s);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        // code here
+        ArrayList<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer>map = new HashMap<>();
+        int sum = 0;
+        map.put(0, -1);
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum - s)) {
+                int startIndex = map.get(sum - s)+1;
+                int endIndex = i + 1;
+                result.add(startIndex + 1); 
+                result.add(endIndex); 
+                return result;
+            }
+            map.put(sum, i);
+            
+        }
+        result.add(-1);
+        return result;
+}
+}
